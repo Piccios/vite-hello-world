4db7a9a6406e4f062ad39ad96eae36e2f39edeba
Piccio <lorenzo.hl.picchi@gmail.com>
2024-06-09 23:12:44 +0200

    set a jumbotron with a funny .webp

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script>
 import AppTitle from './components/AppTitle.vue'
+import AppHero from './components/AppHero.vue'
 export default {
   components:
   {
-    AppTitle
+    AppTitle,
+    AppHero
   },
   data() {
     return {
@@ -20,7 +22,14 @@ export default {
 </script>
 
 <template>
+  <header>
     <AppTitle />
+  </header>
+<main>
+  <section class="jumbotron">
+    <AppHero />
+  </section>
+</main>
 
 </template>
 
--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -6,7 +6,7 @@
 <template>
     <main>
         <section class="jumbotron">
-            
+            <img src="../giphy.webp" alt="What a wonderful day!!">
         </section>
     </main>
 </template>
